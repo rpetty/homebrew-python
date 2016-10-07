f785693e6e75da9b9fddfa18a0ca67f1157b7c3d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-07 11:20:36 +0100

    numpy: update 1.11.2 bottle.

--- numpy.rb
+++ numpy.rb
@@ -7,9 +7,9 @@ class Numpy < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "714347847f02820ee7c921589180b7ce39f608f9215e9e3cb4de12934ad2dd66" => :el_capitan
-    sha256 "1b09b4fefd131daf662cc79c3fa0247dc84385f2d8949a5967fc05325f2505d7" => :yosemite
-    sha256 "36425c6621f82a9090ef84b165ec33df88d4d92f5b8cdbaa05143c31be756933" => :mavericks
+    sha256 "653564e102fc276673a648db00a8409de58c2d405e3f009fe2fca48341dc546b" => :sierra
+    sha256 "adcc6904722700f4540e6d3346d06ad183dbb44103840cbf0ba2907e331878e2" => :el_capitan
+    sha256 "5f4a1549cf8d89437dabce3bcc7f2f7e2ea7ab22a328824d0c9efe4427a52c31" => :yosemite
   end
 
   option "without-python", "Build without python2 support"
